367f0c3a543880fd488dd55c332eab979f319fce
emanuelsarapura214-cmyk <emanuelsarapura214@gmail.com>
2026-03-31 14:41:15 -0300

    HOLA

---
+++ src/practico_1/Practica_2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practico_1;
+
+/**
+ *
+ * @author emanu
+ */
+public class Practica_2 {
+    public static void main(String[] args) {
+        
+        String nombreCompleto="emanuel sarapura";
+        System.out.println("Generador de gmail");
+        System.out.println("nombre de usuerio "  +nombreCompleto);
+        char arroba= '@';
+        String user="ema "; 
+        
+        String nombreEmpresa = "cen tech";
+        System.out.println("nombre de empresa " + nombreEmpresa);
+        
+        String domicilio ="CEN.TECH.COM.AR";
+        System.out.println("mi domicilio"  +domicilio);
+        
+        System.out.println("crear gmail");
+        System.out.println("bombreCompleto+nombreEmpresa");
+        
+    }
+    
+}
---
+++ src/practico_1/Practico_1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practico_1;
+
+/**
+ *
+ * @author emanu
+ */
+public class Practico_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombreCompleto = "emanuelsarapura";
+        int edad = 19 ;
+        double altura = 1.55 ;
+        String paisOrigen = "argentina";
+        char StadoCivil = 's';
+        
+        System.out.println("mi nombre " +nombreCompleto) ;
+        System.out.println("mi edad "  +edad);
+        System.out.println("mi altura "  +altura);
+        System.out.println("mi pais "  + paisOrigen);
+        System.out.println("mi estado "  +StadoCivil);
+       
+        
+ 
+      
+    }
+    
+}
